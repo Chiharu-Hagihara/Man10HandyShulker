aaad412fce3b7509e3757c92a269e2b1e3309599
chiharu <chiharuhagihara803@gmail.com>
2022-08-19 12:36:28 +0900

    initial commit

---
+++ src/main/java/red/man10/man10handyshulker/Listener.kt
@@ -0,0 +1,7 @@
+package red.man10.man10handyshulker
+
+import org.bukkit.event.Listener
+
+class Listener : Listener {
+
+}
---
+++ src/main/java/red/man10/man10handyshulker/Man10HandyShulker.kt
@@ -0,0 +1,11 @@
+package red.man10.man10handyshulker
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Man10HandyShulker : JavaPlugin() {
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+    }
+}
