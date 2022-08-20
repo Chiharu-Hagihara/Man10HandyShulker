f2da99e026466317f662217fb18b39d3f4319ff6
chiharu <chiharuhagihara803@gmail.com>
2022-08-20 23:44:53 +0900

    feat: open shulkerbox

--- src/main/java/red/man10/man10handyshulker/Listener.kt
+++ src/main/java/red/man10/man10handyshulker/Listener.kt
@@ -1,7 +1,47 @@
 package red.man10.man10handyshulker
 
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.block.ShulkerBox
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.BlockStateMeta
+import red.man10.man10handyshulker.Man10HandyShulker.Companion.prefix
+
 
 class Listener : Listener {
+    @EventHandler
+    fun onClick(e: PlayerInteractEvent) {
 
+        if (e.action != Action.RIGHT_CLICK_AIR) return
+
+        val p = e.player
+
+        val itemInMainhand = p.inventory.itemInMainHand
+        val itemInOffhand = p.inventory.itemInOffHand
+
+        if (itemInMainhand.type != Material.SHULKER_BOX) return
+
+        val item: ItemStack = itemInMainhand
+        if (item.itemMeta is BlockStateMeta) {
+            val im = item.itemMeta as BlockStateMeta
+            if (im.blockState is ShulkerBox) {
+                val shulker = im.blockState as ShulkerBox
+                val inv = Bukkit.createInventory(null, 27, Component.text(prefix))
+                inv.contents = shulker.inventory.contents
+                p.openInventory(inv)
+            }
+        }
+
+    }
+
+    @EventHandler
+    fun onClose(e: InventoryCloseEvent) {
+
+    }
 }
--- src/main/java/red/man10/man10handyshulker/Man10HandyShulker.kt
+++ src/main/java/red/man10/man10handyshulker/Man10HandyShulker.kt
@@ -4,8 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class Man10HandyShulker : JavaPlugin() {
     override fun onEnable() {
+        plugin = this
+
+        server.pluginManager.registerEvents(Listener(), plugin)
     }
 
     override fun onDisable() {
+    }
+
+    companion object {
+        lateinit var plugin: Man10HandyShulker
+
+        const val prefix = "§e[&dM&fa&an&f10§5HandyShulker§e]§r"
     }
 }
